9edd7e6b3b4fb59fb49349102e576867b56cf70a
Sveinbjorn10 <Sveinbjorn10@hotmail.com>
2018-09-21 20:57:53 +0000

    Forrit virkar en þarf að laga input shares

---
+++ stockPrice.py
@@ -0,0 +1,35 @@
+condition = True
+
+def shares():
+    return int(input("Enter a number of shares: "))
+
+def price(shares):
+    dollar, nume, deno = input("Enter price (dollars, numerator, denominator): ").split()
+    unit_price = int(dollar) + (int(nume)/int(deno))
+    total_price = round((unit_price * int(shares)), 2)
+    output = str(shares) + " shares with market price " + dollar + " " + nume + "/" + deno + " have value $" + str(total_price)
+    print(output)
+
+def continuation():
+    while True:
+        us_inp = input("Continue: ")
+        if (us_inp == "y") or (us_inp == "Y"):
+            return True
+        elif (us_inp == "n") or (us_inp == "N"): 
+            return False
+        else:
+            pass
+
+while condition:
+    try:
+        share_num = shares()
+        while True:
+            try:
+                price(share_num)
+                break
+            except:
+                print("Invalid price!")
+    except:
+        print("Invalid number!")
+    condition = continuation()
+
